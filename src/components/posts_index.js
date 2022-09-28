c733f721f8976e50a1120ea1b22e1fe828fc271b
Jason Gornall <gornall.jason@gmail.com>
2022-09-27 18:53:22 -0700

    Update posts_index.js

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 import { fetchPosts } from "../actions";
-
+// wakka
 class PostsIndex extends Component {
   componentDidMount() {
     this.props.fetchPosts();
